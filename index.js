e1989555e9e31e7c8c5c205281f377f0fecc218a
Branden Mosley <branden.mosley1337@gmail.com>
2018-08-07 20:46:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,3 +8,8 @@ function updateDriverWithKeyAndValue(obj, key, value) {
 function destructivelyUpdateDriverWithKeyAndValue(obj, key, value) {
   return Object.assign(obj, {[key]:value});
 }
+
+function deleteFromDriverByKey(obj, key) {
+  const newObj = Object.assign({}, obj);
+  delete newObj.key;
+}
